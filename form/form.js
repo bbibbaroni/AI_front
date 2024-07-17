2329294c62f25765855f7ecb4ee391b60a187045
sunz8281 <sunz8281@gmail.com>
2024-07-18 07:33:52 +0900

    error fixed

--- form/form.js
+++ form/form.js
@@ -2,17 +2,21 @@ $diabetes = document.getElementById('diabetes');
 $vagetarian = document.getElementById('vegetarian');
 $allerge = document.getElementById('allerge');
 $hatefood = document.getElementById('hatefood');
+$img = document.getElementById('contentsImg');
 
 $diabetes.style.display="block";
 function showVegetarian(){
     $diabetes.style.display="none";
     $vagetarian.style.display="block";
+    $img.style.backgroundImage = "url('../images/form2-background.png')";
 }
 function showAllerge(){
     $vagetarian.style.display="none";
     $allerge.style.display="block";
+    $img.style.backgroundImage = "url('../images/form3-background.png')";
 }
 function showHatefood(){
     $allerge.style.display="none";
     $hatefood.style.display="block";
+    $img.style.backgroundImage = "url('../images/form4-background.png')";
 }
